2b2215fd9d3637d5c42d29d1257d2adcacb3e060
Weihu Chen <cctiger36@gmail.com>
2013-11-12 11:20:58 +0900

    fix spec

--- spec/model_observer/querying_spec.rb
+++ spec/model_observer/querying_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 module ModelObserver
   describe "#find_by_sql" do
     it "should publish notifications of instantiation when #find_by_sql called" do
+      Author.all.to_a
       expect(ActiveSupport::Notifications.instrumenter).to receive(:instrument).with("instantiate.model_observer", kind_of(Hash))
       Author.all.to_a
     end
